aa59212e2a69f1bfb4058f61bb0ead17459ad8e9
Alex Bishop <alexbishopffxiv@gmail.com>
2021-11-30 10:01:55 -0800

    Reformat client intent setter

--- index.js
+++ index.js
@@ -5,23 +5,7 @@ import { commands } from "./commands/commands.js";
 const CLIENT_SECRET = fs.readFileSync("./.CLIENT_SECRET", "utf8");
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_INTEGRATIONS,
-    Intents.FLAGS.GUILD_WEBHOOKS,
-    Intents.FLAGS.GUILD_INVITES,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_MESSAGE_TYPING,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
-  ],
+  intents: Object.keys(Intents.FLAGS),
 });
 
 client.on("messageCreate", function (message) {
