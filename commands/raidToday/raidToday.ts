dca543792bb61518124de371314f11795763690e
Fladorius <aaronpena@gmail.com>
2021-11-30 20:15:38 -0600

    Adding logic to properly tell us if we actually raid.

--- commands/raidToday/raidToday.ts
+++ commands/raidToday/raidToday.ts
@@ -1,7 +1,29 @@
 import { Message } from "../../models/message";
+import { formatDuration, intervalToDuration } from "date-fns";
+
+const SAVAGE_START = new Date(2022, 0, 4, 1);
+const RAID_DAYS = [2, 3, 4];
 
 export default function raidToday(message: Message) {
   if (message.hasTextMatch("do we raid today")) {
-    message.respond("no");
+    var currentTime = new Date()
+    if (currentTime < SAVAGE_START || !RAID_DAYS.includes(currentTime.getDay())) {
+      message.respond("no");
+      return;
+    }
+
+    var todaysRaidTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), 17, 30);
+    if (todaysRaidTime < currentTime) {
+      message.respond("we already raided");
+      return;
+    }
+
+    message.respond(`yes, we raid in ${formatDuration(
+      intervalToDuration({
+        start: new Date(),
+        end: todaysRaidTime,
+      }),
+      { delimiter: ", " }
+    )}`);
   }
 }
